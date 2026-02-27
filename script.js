106ff8bcc9a1575baa3fff09cef18ec8fddcd0de
otsuguak <cesar@lumengroup.com.co>
2026-02-27 16:34:41 -0500

    27/02/2026
    Ajuste scrip navegador

--- script.js
+++ script.js
@@ -6,14 +6,7 @@ const supabaseUrl = 'https://rqjfaztnaktizrgllhna.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxamZhenRuYWt0aXpyZ2xsaG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxMzk1NjksImV4cCI6MjA4NzcxNTU2OX0.cb6LSWq5YZ7BKRdBx2VoeD-m1gUonfpU_MJemaTSB3U';
 
 // Solo una declaración del cliente, con los parámetros de compatibilidad total
-const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    storage: window.localStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true
-  }
-});
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Variables Globales
 let usuarioActual = null;
